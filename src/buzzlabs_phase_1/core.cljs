13c3beff9b3329bda99b8a3c8860a2b4396ccac9
Felipe Domingues <felipeeu@yahoo.com.br>
2025-06-09 19:26:17 -0300

    Add a fix in lein re-frame boilerplate in order to keep react in last version

--- src/buzzlabs_phase_1/core.cljs
+++ src/buzzlabs_phase_1/core.cljs
@@ -1,24 +1,32 @@
 (ns buzzlabs-phase-1.core
   (:require
-   [reagent.dom :as rdom]
+   [reagent.dom.client :as rdom-client]
    [re-frame.core :as re-frame]
    [buzzlabs-phase-1.events :as events]
    [buzzlabs-phase-1.views :as views]
-   [buzzlabs-phase-1.config :as config]
-   ))
-
+   [buzzlabs-phase-1.config :as config]))
 
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
 
-(defn ^:dev/after-load mount-root []
+(defonce root-container
+  (rdom-client/create-root (js/document.getElementById "app")))
+
+(defn mount-ui
+  []
+  (rdom-client/render root-container [views/main-panel]))
+
+(defn ^:dev/after-load clear-cache-and-render!
+  []
+  ;; The `:dev/after-load` metadata causes this function to be called
+  ;; after shadow-cljs hot-reloads code. We force a UI update by clearing
+  ;; the Reframe subscription cache.
   (re-frame/clear-subscription-cache!)
-  (let [root-el (.getElementById js/document "app")]
-    (rdom/unmount-component-at-node root-el)
-    (rdom/render [views/main-panel] root-el)))
+  (mount-ui))
+
 
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
-  (mount-root))
+  (mount-ui))
